62e175305703380cc4fa62863bdfb5d97208218f
Serdar <serdar@gmail.com>
2022-04-20 12:57:26 +0200

    edit

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouter = () => {
-
-   const user = JSON.parse(sessionStorage.getItem("user"))
-
-  return user? <Outlet/> : <Navigate to = "/"/>
-}
-
-export default PrivateRouter
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  //sessionStorage a gömülmüş user varsa getir, varsa içerde hangi sayfa denildiyse oraya git, yoksa login
+  return user ? <Outlet /> : <Navigate to="/" />;
+};
+//  <Outlet />; = Route nin içindeki döndürülecek component, child
+export default PrivateRouter;
